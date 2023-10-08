2e902350aa99926cca9ae6359b05477d21ff798e
Syed Faisal Abrar <syedfaisalabrar100@gmail.com>
2023-10-08 17:36:38 +0500

    updates

--- src/components/loader.js
+++ src/components/loader.js
@@ -5,7 +5,7 @@ export default class loader extends Component {
   render() {
     return (
       <div className='text-center'>
-        <img src={giphy} alt='giphy' style={{filter : 'invert(100%)', width :'350px'}}></img>
+        <img src={giphy} alt='giphy' style={{filter : 'invert(100%)', width :'150px'}}></img>
       </div>
     )
   }
